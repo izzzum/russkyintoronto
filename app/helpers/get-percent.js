6122534e9fadd0a6403a8642894059f4d4bacd22
izzzum\izum <izum88@gmail.com>
2017-04-05 14:28:18 -0400

    fixed styles and precision for the percent bar on stats

--- app/helpers/get-percent.js
+++ app/helpers/get-percent.js
@@ -1,7 +1,17 @@
 import Ember from 'ember';
 
-export function getPercent(params, args) {  
-  return (args.fraction/args.total*100).toPrecision(5);
+export function getPercent(params, args) {
+  let result = (args.fraction/args.total*100);
+  if(parseInt(result).toString === 1){
+    result = result.toPrecision(3);
+  }
+  else if(parseInt(result).toString === 2){
+    result = result.toPrecision(4);
+  }
+  else{
+    result = result.toPrecision(3);
+  }
+  return result;
 }
 
 export default Ember.Helper.helper(getPercent);
