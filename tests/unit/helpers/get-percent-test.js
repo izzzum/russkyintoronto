0e02fe4408a41c2fc9147597c78401faf7545acf
izzzum\izum <izum88@gmail.com>
2017-04-05 03:18:44 -0400

    modified styles and text content

---
+++ tests/unit/helpers/get-percent-test.js
@@ -0,0 +1,12 @@
+
+import { getPercent } from 'my-app/helpers/get-percent';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | get percent');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = getPercent([42]);
+  assert.ok(result);
+});
+
