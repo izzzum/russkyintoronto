853d9d52b9da0cb4a17c2433485f48d5fda41e69
izzzum\izum <izum88@gmail.com>
2017-04-11 23:33:55 -0400

    nav menu for post page

--- app/controllers/post.js
+++ app/controllers/post.js
@@ -1,4 +1,16 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+    actions: {
+        scrollTop(){
+        Ember.$('body').stop().animate({
+            scrollTop: (Ember.$('.page-header').offset().top-100)
+        }, 1000);
+    },
+    scrollBot(){
+        Ember.$('body').stop().animate({
+            scrollTop: (Ember.$('.page-footer').offset().top-100)
+        }, 1000);
+        }
+    }
 });
