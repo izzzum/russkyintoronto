8d5cb36c7b97d2807165984c5eaa469b3561bc30
izzzum\izum <izum88@gmail.com>
2017-04-20 22:17:55 -0400

    if condition for youtube ajax; footer tag change

--- app/components/page-footer.js
+++ app/components/page-footer.js
@@ -1,7 +1,8 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-    classNames: ['page-footer'],
+    classNames: ['page-footer footer'],
+    tagName: ['footer'],
     attributeBindings: ['id'],
     id: 'footer',
 });
--- app/components/put-video.js
+++ app/components/put-video.js
@@ -12,12 +12,15 @@ export default Ember.Component.extend({
             this.set('loading', true);
             this.set('youtube', true);
             let store = this.get('store');
-            let title = this.get('video.title');
-            store.queryRecord('youtube', {key: 'AIzaSyCVsGkmf4dsvbk3zj0pdijTwuSwYe8Pcmk', part:'snippet', maxResults:1, q: title, type: 'video'}).then(
-                function(result){
-                    _this.set('video.youtube', result);
-                    _this.set('loading', false);
-                });
+            let video = this.get('video');
+            if(Ember.isEmpty(video.youtube)){
+                let title = this.get('video.title');
+                store.queryRecord('youtube', {key: 'AIzaSyCVsGkmf4dsvbk3zj0pdijTwuSwYe8Pcmk', part:'snippet', maxResults:1, q: title, type: 'video'}).then(
+                    function(result){
+                        _this.set('video.youtube', result);
+                        _this.set('loading', false);
+                    });
+            }
         }
     },
     actions:{
