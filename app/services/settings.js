124a4699cbdb0a13df4aca6601e22c4c25a35388
izzzum\izum <izum88@gmail.com>
2017-05-07 16:08:19 -0400

    title and readme fix

--- app/services/settings.js
+++ app/services/settings.js
@@ -6,7 +6,5 @@ export default Ember.Service.extend({
     defaultStatsAmount: 50,
     defaultStatsButtonVal: 100,
     maxStatsValue: 1000,
-    postLoadAmount: 8,
-    header: 'Russky in Toronto<sup><i class="fa fa-vk"></i></sup>',
-    footer: 'You\'re not making it easy <i class="fa fa-copyright" aria-hidden="true"></i> 2017'
+    postLoadAmount: 8
 });
